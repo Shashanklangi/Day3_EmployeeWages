59e19ba92de350c76ad8035bf3a889cda71e7564
Shashank <langishashank@gmail.com>
2022-05-21 01:11:50 +0530

    [Shashank]:Total Wage when queried by company.

--- EmployeeWages/EmployeeWages/Program.cs
+++ EmployeeWages/EmployeeWages/Program.cs
@@ -16,7 +16,10 @@ namespace EmployeeWages
             empWageComputation.CalculateEmpWage("mahindra");
             empWageComputation.AddCompany("Tesla", 60, 8, 7, 10, 200);
             empWageComputation.CalculateEmpWage("tesla");
-            empWageComputation.ViewWage();
+
+            Console.WriteLine("Enter the Company name");
+            string company = Convert.ToString(Console.ReadLine());
+            empWageComputation.GetInfo();
 
         }
     }
